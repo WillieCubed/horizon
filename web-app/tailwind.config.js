db4fa3ca0bf59172c733e261ff8da84f3cb21f62
Willie Chalmers III <willie@williecubed.me>
2022-11-13 02:40:17 -0600

    Update background styles

--- web-app/tailwind.config.js
+++ web-app/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        primary: '#F9F7F0',
+      },
+    },
   },
   plugins: [],
 }
